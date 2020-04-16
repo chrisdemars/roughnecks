978ddfa8fce362d35dc787c1f341984e9cb34c49
Chris DeMars <cdemars86@gmail.com>
2020-04-15 22:54:21 -0600

    adds and deletes stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
 import Hero from './components/Hero';
 import Testimony from './components/Testimony';
 import Services from './components/Services';
@@ -16,7 +15,6 @@ function App() {
   const breakpoint = isLarge ? 'large' : isMedium ? 'medium' : 'small';
   return (
     <div className={`app-container ${breakpoint}`}>
-      <Header />
       <Hero />
       <Testimony />
       <Services />
