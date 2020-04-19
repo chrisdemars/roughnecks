a19280ad3af68b9b62d32cb499c0a6f403bb7fb2
Chris DeMars <cdemars86@gmail.com>
2020-04-19 00:04:48 -0600

    more things

---
+++ src/components/BookButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BookButton = () => {
+  return (
+    <a
+      href="https://square.site/book/QQEE304K2GT87/roughnecks-barbershop-livonia-mi"
+      className="btn-book"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Reserve Your Spot
+    </a>
+  );
+};
+
+export default BookButton;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import logo from '../images/circle-logo.svg';
 
+import BookButton from './BookButton';
+
 const Hero = () => {
   return (
     <div className="hero-wrapper">
       <div className="wrapper">
         <section className="hero">
+          <div className="about-section">
+            <h1>Welcome to Roughnecks!</h1>
+            <p>
+              Here at Roughnecks, we offer a wide array of men’s cuts and styles
+              along with straight razor work to make any haircut service
+              complete. Book your appointment now!
+            </p>
+            <p>Appointments are encouraged and guaranteed.</p>
+            <p>Walk-ins are welcome however, may result in waiting.</p>
+            <BookButton />
+          </div>
           <div className="logo-section">
             <img src={logo} alt="Roughnecks Barbershop Logo" />
-          </div>
-          <div className="about-section">
-            <h1>Welcome to Roughnecks Barbershop!</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
-              laudantium repudiandae maiores possimus eligendi, nostrum quaerat
-              ratione maxime a! Illo.
-            </p>
           </div>
         </section>
       </div>
