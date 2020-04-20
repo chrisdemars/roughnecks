e75a8a024cbee2b072153fc5de72c76f20e15d7f
Chris DeMars <cdemars86@gmail.com>
2020-04-20 00:07:48 -0600

    map fun. got it working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './components/Hero';
 import Testimony from './components/Testimony';
 import Services from './components/Services';
+import Map from './components/Map';
 import PrimaryFooter from './components/PrimaryFooter';
 import SecondaryFooter from './components/SecondaryFooter';
 
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <Testimony />
       <Services />
+      <Map />
       <PrimaryFooter />
       <SecondaryFooter />
     </div>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,1 +1,36 @@
+import React, { useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import mapInfo from './data/map.json';
+import MapMarker from '../images/marker.png';
 
+const Map = () => {
+  const [viewport] = useState({
+    center: [-83.353776, 42.42743],
+    latitude: 42.42743,
+    longitude: -83.353776,
+    width: '100vw',
+    height: '50vh',
+    zoom: 15,
+  });
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      >
+        {mapInfo.features.map((map) => (
+          <Marker
+            key={map}
+            latitude={map.geometry.coordinates[1]}
+            longitude={map.geometry.coordinates[0]}
+          >
+            <img src={MapMarker} alt="Shop marker" />
+          </Marker>
+        ))}
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default Map;
