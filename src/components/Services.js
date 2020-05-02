d3ab54502f13f7805745b78666acce9704e85050
Chris DeMars <cdemars86@gmail.com>
2020-05-01 21:35:29 -0600

    stuff

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <section className="services">
-      <h2>Services Section</h2>
+    <section className="services-wrapper">
+      <div className="wrapper">
+        <div className="interior-image-section">
+          <img src={'./services.jpg'} alt="" />
+        </div>
+        <div className="services-section">
+          <h2>Services</h2>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
