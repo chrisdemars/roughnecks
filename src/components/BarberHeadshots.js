c894ea4e239be8e8e17ad44a37ec34b496545175
Chris DeMars <cdemars86@gmail.com>
2020-07-18 23:55:54 -0400

    creates component

--- src/components/BarberHeadshots.js
+++ src/components/BarberHeadshots.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const BarberHeadshots = () => {
-  return <div></div>;
+  return (
+    <section className="headshots-wrapper">
+      <h2>Meet the barbers</h2>
+      <div className="wrapper">
+        <div className="">
+          <img src="" alt="" />
+          <h3>Derek Favaro</h3>
+          <p>BIO</p>
+          <button>Book</button>
+        </div>
+        <div className="">
+          <img src="" alt="" />
+          <h3>Dere's Sister</h3>
+          <p>BIO</p>
+          <button>Book</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BarberHeadshots;
