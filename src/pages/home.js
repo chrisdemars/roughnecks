a4fdf9d34da210c42d373402e0a0261f8fc8c72a
Chris <chris@chrisdemars.com>
2022-01-26 22:48:38 -0500

    removes map. adds new bios

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import BarberHeadshots from '../components/BarberHeadshots';
-import Map from '../components/Map';
 import PrimaryFooter from '../components/PrimaryFooter';
 import SecondaryFooter from '../components/SecondaryFooter';
 import NotificationBanner from '../components/NotificationBanner';
 
-function Home (){
-
+function Home() {
   return (
-      <>
-        <NotificationBanner/>
-        <Hero />
-        <BarberHeadshots />
-        <Map />
-        <PrimaryFooter />
-        <SecondaryFooter />
-      </>
+    <>
+      <NotificationBanner />
+      <Hero />
+      <BarberHeadshots />
+      <PrimaryFooter />
+      <SecondaryFooter />
+    </>
   );
 }
 
