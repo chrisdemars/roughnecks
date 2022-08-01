e758394fb91d66fa1171ddc63494c38501a6a26c
Chris <chris@chrisdemars.com>
2022-08-01 19:24:25 -0400

    adds new link for services

--- src/components/BookButton.js
+++ src/components/BookButton.js
@@ -3,11 +3,10 @@ import React from 'react';
 const BookButton = () => {
   return (
     <a
-      href="https://square.site/book/QQEE304K2GT87/roughnecks-barbershop-livonia-mi"
-      className="btn-book"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+      href='https://squareup.com/appointments/book/hfjzi9z2ey4hnc/QQEE304K2GT87/services'
+      className='btn-book'
+      target='_blank'
+      rel='noopener noreferrer'>
       Book Your Spot
     </a>
   );
